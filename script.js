1388ee00015cba3919053074b50b0da8134f7ef7
Rivu Khoda <rivu.khoda@gmail.com>
2017-10-26 22:53:50 -0400

    New: Can now add items to the task list.

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+window.addEventListener('load', init);
 
+function init() {
+    document.getElementById("button").addEventListener('click', addItem);
+}
+
+
+function addItem() {
+    var userInput = document.getElementById("input");
+    var newContent = document.createTextNode(userInput.value);
+
+    var checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+
+    var label = document.createElement("label");
+    label.appendChild(checkBox);
+    label.appendChild(newContent);
+
+
+    var newLi = document.createElement("li");
+    newLi.appendChild(label);
+
+    document.getElementById("list").appendChild(newLi);
+    userInput.value = "";
+
+}
+
+function removeItem() {
+
+}
+
+function editItem() {
+
+}
