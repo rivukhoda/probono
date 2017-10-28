724d66fa3d35c428e3c8cbf03ddc12fce39afe5b
Rivu Khoda <rivu.khoda@gmail.com>
2017-10-28 19:46:48 -0400

    New: Can load list of task items from external server.

--- script.js
+++ script.js
@@ -2,12 +2,32 @@ window.addEventListener('load', init);
 
 function init() {
     document.getElementById("button").addEventListener('click', addItem);
+    displayItems();
 }
 
 
-function addItem() {
-    var userInput = document.getElementById("input");
-    var newContent = document.createTextNode(userInput.value);
+//function addItem() {
+//    var userInput = document.getElementById("input");
+//    var newContent = document.createTextNode(userInput.value);
+//
+//    var checkBox = document.createElement("input");
+//    checkBox.setAttribute("type", "checkbox");
+//    checkBox.addEventListener('change', removeItem);
+//
+//    var label = document.createElement("label");
+//    label.appendChild(newContent);
+//
+//    var newLi = document.createElement("li");
+//    newLi.appendChild(checkBox);
+//    newLi.appendChild(label);
+//
+//    document.getElementById("list").appendChild(newLi);
+//    userInput.value = "";
+//
+//}
+
+function addItem(description) {
+    var newContent = document.createTextNode(description);
 
     var checkBox = document.createElement("input");
     checkBox.setAttribute("type", "checkbox");
@@ -21,9 +41,9 @@ function addItem() {
     newLi.appendChild(label);
 
     document.getElementById("list").appendChild(newLi);
-    userInput.value = "";
 
 }
+
 
 function removeItem(event) {
     event.target.parentNode.remove();
@@ -33,16 +53,93 @@ function editItem() {
 
 }
 
+function createTask() {
+    var data = {
+        "name": getName(),
+        "email": getEmail(),
+        "description": getDescription(),
+        "timeEstimate": getTimeEstimate(),
+        "dueDate": getDueDate()
+    };
+
+    var url;
+    makePostRequest(data);
+}
+
+function displayItems() {
+
+    var promisedTasks = getTasks();
+    promisedTasks.then(
+        function (tasks) {
+            for (var i = 0; i < tasks.length; i++) {
+                addItem(tasks[i].description);
+            }
+        }
+    ).catch(function(e) {
+        console.log(e);
+    })
+}
+
+function getTasks() {
+    var url = "https://my.api.mockaroo.com/tasks.json?key=835b6af0";
+    return makeGetRequest(url);
+}
+
+function getDescription() {
+    return document.getElementById("input");
+}
+
 function getEmail() {
-    var email = document.getElementById("email");
+    return document.getElementById("email");
 
 }
 
 function getName() {
-    var name = document.getElementbyId("name");
+    return document.getElementById("name");
 
 }
 
 function getTimeEstimate() {
-    var timeEstimate = document.getElementById("timeEstimate");
+    return document.getElementById("number");
+}
+
+function getDueDate() {
+    return document.getElementById("date");
+}
+
+function setWaitTime() {
+
+}
+
+function makePostRequest(url, data) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json);
+        }
+    };
+    xhr.send(JSON.stringify(data));
+}
+
+function makeGetRequest(url) {
+    return new Promise(
+        function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            //xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onload = function () {
+                var json = JSON.parse(xhr.responseText);
+                resolve(json);
+            };
+            xhr.onerror = function() {
+                reject(xhr.statusText);
+            };
+            xhr.send();
+
+        }
+    )
+
 }
