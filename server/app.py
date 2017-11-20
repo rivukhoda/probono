c27a22793eccdd73577c7690612c58740662ea9f
Rivu Khoda <rivu.khoda@gmail.com>
2017-11-19 19:47:11 -0500

    New: Added endpoints for task, list, user and session objects.

--- server/app.py
+++ server/app.py
@@ -1,9 +1,56 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return
+
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    return
+
+
+@app.route('/tasks/<id>', methods=['DELETE'])
+def delete_task():
+    return
+
+
+@app.route('/lists', methods=['GET'])
+def get_lists():
+    return
+
+
+@app.route('/lists', method=['POST'])
+def create_list():
+    return
+
+
+@app.route('/lists/<id>', methods=['DELETE'])
+def delete_list():
+    return
+
+
+@app.route('/session', methods=['POST'])
+def login_user():
+    return
+
+
+@app.route('/session', methods=['DELETE'])
+def logout_user():
+    return
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    return
 
 
 if __name__ == "__main__":
