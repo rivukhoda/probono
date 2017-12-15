28fddd10e8c7eb4e0300d4d3650c1fb91da4b577
Rivu Khoda <rivu.khoda@gmail.com>
2017-12-14 20:06:57 -0500

    New: Created http utility class

---
+++ client/scripts/http-utils.js
@@ -0,0 +1,81 @@
+export {makeGetRequest, makePostRequest, makeDeleteRequest, makePutRequest}
+
+function makePostRequest(url, data) {
+    return new Promise(
+        function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onload = function () {
+                var json = JSON.parse(xhr.responseText);
+                resolve(json);
+            };
+            xhr.onerror = function () {
+                reject(xhr.statusText);
+            };
+            xhr.send(JSON.stringify(data));
+
+        }
+    )
+
+
+}
+
+function makeGetRequest(url) {
+    return new Promise(
+        function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onload = function () {
+                var json = JSON.parse(xhr.responseText);
+                resolve(json);
+            };
+            xhr.onerror = function () {
+                reject(xhr.statusText);
+            };
+            xhr.send();
+
+        }
+    )
+}
+
+function makeDeleteRequest(url) {
+    return new Promise(
+        function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("DELETE", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onload = function () {
+                var json = JSON.parse(xhr.responseText);
+                resolve(json);
+            };
+            xhr.onerror = function () {
+                reject(xhr.statusText);
+            };
+            xhr.send();
+
+        }
+    )
+}
+
+function makePutRequest(url, data) {
+    return new Promise(
+        function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("PUT", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onload = function () {
+                var json = JSON.parse(xhr.responseText);
+                resolve(json);
+            };
+            xhr.onerror = function () {
+                reject(xhr.statusText);
+            };
+            xhr.send(JSON.stringify(data));
+
+        }
+    )
+
+
+}
