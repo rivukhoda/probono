4fb541c2e11fde5857da2355cf66af56f61815f7
Rivu Khoda <rivu.khoda@gmail.com>
2018-02-19 21:49:15 -0500

    New: Webpack configurations are setup to bundle scripts in a specific manner

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './scripts/script.js',
+    entry: {
+        main: './scripts/main.js',
+        admin: './scripts/admin.js',
+        auth: './scripts/auth.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common'
+        })
+    ],
     output: {
-        filename: '../dist/bundle.js',
-        path: path.resolve(__dirname, 'pages')
-    }
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    watch: true
 };
